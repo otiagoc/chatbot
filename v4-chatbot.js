de37a0920d83c9071efee735d78701c1f4698f9d
otiagoc <99370869+otiagoc@users.noreply.github.com>
2025-01-30 12:18:18 +0000

    v4.1-chatbot.js

--- v4-chatbot.js
+++ v4-chatbot.js
@@ -363,8 +363,8 @@
             </div>
             <div class="chat-messages"></div>
             <div class="chat-input">
-                <textarea placeholder="Type your message here..." rows="1"></textarea>
-                <button type="submit">Send</button>
+                <textarea placeholder="Escreva aqui..." rows="1"></textarea>
+                <button type="submit">Enviar</button>
             </div>
             <div class="chat-footer">
                 <a href="${config.branding.poweredBy.link}" target="_blank">${config.branding.poweredBy.text}</a>
